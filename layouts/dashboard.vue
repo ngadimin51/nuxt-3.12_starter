745d6e1f201a5a84a2e5a174462b3c9619159cb7
tofik <poconggoyangkayang@gmail.com>
2024-07-11 23:05:55 +0700

    minor

---
+++ layouts/dashboard.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppHeader from '@/components/AppHeaderDashboard';
+import AppFooter from '@/components/AppFooter';
+</script>
+
+<template>
+  <div>
+    <AppHeader />
+    <div class="p-8">
+      <slot />
+    </div>
+    <AppFooter />
+  </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'dashboard'
+})
+</script>
+
 <template>
   <section>
     <p>This page will be displayed at the /about route.</p>
